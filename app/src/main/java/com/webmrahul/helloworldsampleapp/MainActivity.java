397b3fce985f1ccf48e38bc5dab7625bf5eba5ce
Rahul <webmrahul@gmail.com>
2018-12-27 13:49:43 +0530

    First Commit

---
+++ app/src/main/java/com/webmrahul/helloworldsampleapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.webmrahul.helloworldsampleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
